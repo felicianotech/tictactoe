d0325faa8454eb6b0b06359dd5bbd0338e534e19
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-06-11 23:11:29 -0400

    WIP

--- tictactoe/main.go
+++ tictactoe/main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Board struct {
+	tiles [3][3]string
+}
+
+func (this *Board) Draw(screen *ebiten.Image) {
+
+	var output string
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			output = fmt.Sprintf("%s %s | ", output, this.tiles[i][j])
+		}
+
+		output = fmt.Sprintf("%s\n", output)
+	}
+
+	ebitenutil.DebugPrint(screen, output)
+}
+
+type Game struct {
+	board      *Board
+	boardImage *ebiten.Image
+}
 
 func (this *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hey Michael")
+
+	if this.boardImage == nil {
+		this.boardImage = ebiten.NewImage(500, 500)
+	}
+
+	this.board.Draw(this.boardImage)
+	screen.DrawImage(this.boardImage, nil)
 }
 
 func (this *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -23,10 +52,14 @@ func (this *Game) Update() error {
 
 func main() {
 
+	game := &Game{
+		board: &Board{},
+	}
+
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Michael Threat")
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
