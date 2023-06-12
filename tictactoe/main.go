4f4ff6915d2f8f68c6a8f1055ec57390ec622b6d
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-06-11 21:40:24 -0400

    WIP

--- tictactoe/main.go
+++ tictactoe/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/repowarden/cli/warden/cmd"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+func (this *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hey Michael")
+}
+
+func (this *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func (this *Game) Update() error {
+	return nil
+}
 
 func main() {
-	cmd.Execute()
+
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Michael Threat")
+
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
